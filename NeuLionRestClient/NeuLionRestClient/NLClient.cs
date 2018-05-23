303985bee1f86e13e1ddd535980e3a6d0dd7d507
Daniele Marconi <d.marconi@im-en.com>
2018-05-23 17:48:35 +0200

    Implementented Auth Token generator

---
+++ NeuLionRestClient/NeuLionRestClient/NLClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace NeuLionRestClient
+{
+    class NLClient
+    {
+        string xAuthKey = string.Empty;
+        string loginName = string.Empty;
+        string apiKey = string.Empty;
+
+        public NLClient()
+        {
+            loginName = "admin";
+            apiKey = "234F3B991AB142A4B64A15BCB82EF97E";
+        }
+
+        // Generate X-Auth key according to the following recipe
+        //  X-Auth-Key = md5(api_key + md5(login_name + api_key + current_unix_time)) 
+        public void GenAuthKey()
+        {
+            string token = string.Empty;
+            Int32 unixTimestamp = 0;
+
+            Debug.WriteLine("START OF ---- XAuth token generation");
+            Debug.Indent();
+
+            unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            Debug.WriteLine(string.Format("API key: {0}", apiKey));
+            Debug.WriteLine(string.Format("Login name: {0}", loginName));
+            Debug.WriteLine(string.Format("Current Unix Time: {0}", unixTimestamp));
+
+            Debug.Unindent();
+            Debug.WriteLine("END OF ---- XAuth token generation");
+
+        }
+
+    }
+}
---
+++ NeuLionRestClient/NeuLionRestClient/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuLionRestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            NLClient client = new NLClient();
+
+            client.GenAuthKey();
+
+            Console.ReadKey();
+        }
+    }
+}
